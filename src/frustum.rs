75240dc95fc6826eae79546e229b1f539ccd39aa
enn3 <arturpeshko39@gmail.com>
2023-06-11 21:58:36 +0200

    Removed spheres test from frustum culler

--- src/frustum.rs
+++ src/frustum.rs
@@ -67,86 +67,26 @@ impl FrustumCuller {
         culler.nx_y = m.y_axis.w + m.y_axis.x;
         culler.nx_z = m.z_axis.w + m.z_axis.x;
         culler.nx_w = m.w_axis.w + m.w_axis.x;
-        //if (allow_test_spheres) {
-        let invl =
-            (culler.nx_x * culler.nx_x + culler.nx_y * culler.nx_y + culler.nx_z * culler.nx_z)
-                .sqrt()
-                .recip();
-        culler.nx_x *= invl;
-        culler.nx_y *= invl;
-        culler.nx_z *= invl;
-        culler.nx_w *= invl;
-        //}
         culler.px_x = m.x_axis.w - m.x_axis.x;
         culler.px_y = m.y_axis.w - m.y_axis.x;
         culler.px_z = m.z_axis.w - m.z_axis.x;
         culler.px_w = m.w_axis.w - m.w_axis.x;
-        //if (allow_test_spheres) {
-        let invl =
-            (culler.px_x * culler.px_x + culler.px_y * culler.px_y + culler.px_z * culler.px_z)
-                .sqrt()
-                .recip();
-        culler.px_x *= invl;
-        culler.px_y *= invl;
-        culler.px_z *= invl;
-        culler.px_w *= invl;
-        //}
         culler.ny_x = m.x_axis.w + m.x_axis.y;
         culler.ny_y = m.y_axis.w + m.y_axis.y;
         culler.ny_z = m.z_axis.w + m.z_axis.y;
         culler.ny_w = m.w_axis.w + m.w_axis.y;
-        //if (allow_test_spheres) {
-        let invl =
-            (culler.ny_x * culler.ny_x + culler.ny_y * culler.ny_y + culler.ny_z * culler.ny_z)
-                .sqrt()
-                .recip();
-        culler.ny_x *= invl;
-        culler.ny_y *= invl;
-        culler.ny_z *= invl;
-        culler.ny_w *= invl;
-        //}
         culler.py_x = m.x_axis.w - m.x_axis.y;
         culler.py_y = m.y_axis.w - m.y_axis.y;
         culler.py_z = m.z_axis.w - m.z_axis.y;
         culler.py_w = m.w_axis.w - m.w_axis.y;
-        //if (allow_test_spheres) {
-        let invl =
-            (culler.py_x * culler.py_x + culler.py_y * culler.py_y + culler.py_z * culler.py_z)
-                .sqrt()
-                .recip();
-        culler.py_x *= invl;
-        culler.py_y *= invl;
-        culler.py_z *= invl;
-        culler.py_w *= invl;
-        //}
         culler.nz_x = m.x_axis.w + m.x_axis.z;
         culler.nz_y = m.y_axis.w + m.y_axis.z;
         culler.nz_z = m.z_axis.w + m.z_axis.z;
         culler.nz_w = m.w_axis.w + m.w_axis.z;
-        //if (allow_test_spheres) {
-        let invl =
-            (culler.nz_x * culler.nz_x + culler.nz_y * culler.nz_y + culler.nz_z * culler.nz_z)
-                .sqrt()
-                .recip();
-        culler.nz_x *= invl;
-        culler.nz_y *= invl;
-        culler.nz_z *= invl;
-        culler.nz_w *= invl;
-        //}
         culler.pz_x = m.x_axis.w - m.x_axis.z;
         culler.pz_y = m.y_axis.w - m.y_axis.z;
         culler.pz_z = m.z_axis.w - m.z_axis.z;
         culler.pz_w = m.w_axis.w - m.w_axis.z;
-        //if (allow_test_spheres) {
-        let invl =
-            (culler.pz_x * culler.pz_x + culler.pz_y * culler.pz_y + culler.pz_z * culler.pz_z)
-                .sqrt()
-                .recip();
-        culler.pz_x *= invl;
-        culler.pz_y *= invl;
-        culler.pz_z *= invl;
-        culler.pz_w *= invl;
-        //}
 
         culler
     }
